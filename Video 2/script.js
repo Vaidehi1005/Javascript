99ad7f896a2e7c6eef31d3afff9bfa04d9ca7ff0
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-25 17:50:37 +0530

    v2

--- Video 2/script.js
+++ Video 2/script.js
@@ -1,7 +1,23 @@
 console.log("Hey this is JS")
 //console ma show thai jase aa o/p have variables banaisu
-var a = 5;//we make a var a whose value is no 
-var b = 6;//we make a var a whose value is no 
-var c = "Vaidehi";//we made a var c that have double quotes means it is string
-console.log(a + b + 8)//here we are printing in console var a+B+8=19
-console.log(typeof a, typeof b, typeof c)//here we get the data type of the each var
+let a = 5;
+//we make a var a whose value is no 
+let b = 6;
+//we make a var a whose value is no 
+let c = "Vaidehi";
+//we made a var c that have double quotes means it is string
+let _a = "Jeet";
+//ek vaar koinu naam _ thi chalu thai sake
+// var 55a = "Rt";js ma koi pn varnu naam no thi na chalu thai
+console.log(a + b + 8)
+//here we are printing in console var a+B+8=19
+console.log(typeof a, typeof b, typeof c)
+//here we get the data type of the each var
+// let aek keyword che var mate mordern js ma and let ek block scope che e just ej block ma chale ena sivai e bija koi block ma nai chale  
+const a1 = 1;//aa const keyword no matlab che a1 ni value constant rese ema change nai kari sakie jm k a1=a1+3 k evu eni value je assign kari ej value rese because a1 constant che
+{
+    let a =66;
+    //as let block scope che ene upar define karyu che so e global var bani gayo
+    console.log(a)
+}
+console.log(a);
