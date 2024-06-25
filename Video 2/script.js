f74361bc281d7ea12d4ed6c9b7428319b090251b
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-25 17:18:56 +0530

    v2

--- Video 2/script.js
+++ Video 2/script.js
@@ -1,1 +1,7 @@
 console.log("Hey this is JS")
+//console ma show thai jase aa o/p have variables banaisu
+var a = 5;//we make a var a whose value is no 
+var b = 6;//we make a var a whose value is no 
+var c = "Vaidehi";//we made a var c that have double quotes means it is string
+console.log(a + b + 8)//here we are printing in console var a+B+8=19
+console.log(typeof a, typeof b, typeof c)//here we get the data type of the each var
