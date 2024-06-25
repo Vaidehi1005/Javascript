a17d38f4e7dc3c9ccdb61449a9ebf8ceda0620aa
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-25 14:14:07 +0530

    u5

--- Video 1/script.js
+++ Video 1/script.js
@@ -1,2 +1,5 @@
 alert("Hello World");
 // here the alert have text inside it while having double quotes and what we write inside it that comes when page gets reload that gives us alert that Hello world 
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is looking like a wow...")
