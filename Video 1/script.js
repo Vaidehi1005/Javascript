474c6d8becdd6c9f420e06367cb4404eccda9c02
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-25 14:01:48 +0530

    u4

--- Video 1/script.js
+++ Video 1/script.js
@@ -1,1 +1,2 @@
-alert("Hello World"); 
+alert("Hello World");
+// here the alert have text inside it while having double quotes and what we write inside it that comes when page gets reload that gives us alert that Hello world 
