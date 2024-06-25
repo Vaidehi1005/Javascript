db8072f40eb8b85b7cca0ef71a718d9cd8b629f0
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-25 14:36:20 +0530

    u7

--- Video 1/script.js
+++ Video 1/script.js
@@ -4,3 +4,6 @@ console.log("Code is running...")
 console.log("Code is also running...")
 console.log("Code is looking like a wow...")
 // console.log is used in js it tells about the how much execution is there in the script this are made by console.log and helps in debugging.aa code ne kese ketlu execution thayu che ane error solve karva ma pn help kare 
+
+var a = prompt("Enter your number")
+//ek variable a banavyo pela alert aavse helo world then e alert ne ok karisu to biji alert aavse prompt ni je ema nakhyu tu k enter your number then ema no enter karvano and ok karisu to var a ni value e no thai jase.
