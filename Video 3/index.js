c0056bfb224e1e6c252b9056cc9a1121dd6cfbaa
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-26 14:48:09 +0530

    v3

--- Video 3/index.js
+++ Video 3/index.js
@@ -1,6 +1,6 @@
 console.log("Hello I am conditional tutorial")
 //console log just print karse node js k console par
-let age = 18;
+let age = 10;
 //ek var banayo age je block scope che 
 // let grace = 2;
 //ek var banayo grace e pn block scope che
@@ -12,7 +12,7 @@ let age = 18;
 // console.log(age ** grace);//exp 3^2=9
 // console.log(age % grace);//aa operator reminder aapse
 
-if (age== 18) {
+if (age!= 18) {
     console.log("You can drive");//if else condtition if age+grace bane 18 thi vadhare hase to if ma k else ma
 }
 else {
