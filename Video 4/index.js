6d7655472b82b176bafd59ac04da24cb817abb21
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-26 17:49:00 +0530

    v3

---
+++ Video 4/index.js
@@ -0,0 +1,24 @@
+console.log("I am a tutorial on loops")
+
+let a = 1;
+// console.log(a);
+// console.log(a+1);
+// console.log(a+2);it is not the better way for the large no
+
+//for-loop
+for (let i = 0; i < 100; i++) {
+    console.log(a + i);
+}
+// for-in loop that gives the key of the object
+let obj = {
+    "name": "Vaidehi",
+    "role": "Programmer",
+    "company": "CodewithHarry AI"
+}
+
+for (const k in obj) {
+    
+        console.log(k);
+
+    
+}
