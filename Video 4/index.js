133e99539180fcfe5de45936257833b84b50ff5d
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-26 18:07:00 +0530

    v3

--- Video 4/index.js
+++ Video 4/index.js
@@ -6,20 +6,27 @@ let a = 1;
 // console.log(a+2);it is not the better way for the large no
 
 //for-loop
-for (let i = 0; i < 100; i++) {
-    console.log(a + i);
-}
-// for-in loop that gives the key of the object
-let obj = {
-    "name": "Vaidehi",
-    "role": "Programmer",
-    "company": "CodewithHarry AI"
-}
-//this for in loop prints the element and the key of the object
-for (const k in obj) {
-    console.log(k);
-}
-//for-of loop is for string ,array,etc
-for (const v of "Vaidehi") {
-console.log(v);
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+// // for-in loop that gives the key of the object
+// let obj = {
+//     "name": "Vaidehi",
+//     "role": "Programmer",
+//     "company": "CodewithHarry AI"
+// }
+// //this for in loop prints the element and the key of the object
+// for (const k in obj) {
+//     console.log(k);
+// }
+// //for-of loop is for string ,array,etc and that prints a whole string in char form
+// for (const v of "Vaidehi") {
+// console.log(v);
+// }
+
+//while-loop
+let i = 0;
+while (i < 6) {
+    console.log(i)
+    i++;
 }
