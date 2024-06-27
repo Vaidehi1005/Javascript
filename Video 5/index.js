9f212d9770d6a2ba11efc794a0f8bf299d86bfab
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-27 14:20:41 +0530

    v5

--- Video 5/index.js
+++ Video 5/index.js
@@ -10,16 +10,16 @@ function nice(name) {
 
 // nice("Parthvi");
 
-function sum(a, b,c=3) {
-    console.log(a,b,c);
-    // console.log(a+b);//this is the void fun which is not returning any value and just printing the value
-    return a + b +c;//this is for returning the integer value when passing the int value and storing into result and then we will print the result
+function sum(a, b, c = 3) {
+    console.log(a, b, c);
+    console.log(a + b);//this is the void fun which is not returning any value and just printing the value
+    return a + b + c;//this is for returning the integer value when passing the int value and storing into result and then we will print the result
 }
-// sum(11,222);
+sum(11, 222);
 
 result1 = sum(22, 10);
-result2 = sum(2);
-result3 = sum(122, 10,2);//passing c is optional if we are passing then it's value would be this
+result2 = sum(2, 9);
+result3 = sum(122, 10, 2);//passing c is optional if we are passing then it's value would be this
 //the fun is used for reusability
 
 console.log("The sum of the number is: ", result1);
