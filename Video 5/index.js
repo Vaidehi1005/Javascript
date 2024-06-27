268511c7050c33609383305aaf5e5d0f9252b36d
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-27 13:55:37 +0530

    v5

--- Video 5/index.js
+++ Video 5/index.js
@@ -1,9 +1,11 @@
 // Functions
-
+//functions are used for writing the code with var we dont have to write again we can reuse the code in the fun just by changing the name
 function nice(name) {
     console.log("Hey " + name + " I am nice!");
     console.log("Hey " + name + " I am fine!");
     console.log("Hey " + name + " your tshirt is nice!");
     console.log("Hey " + name + " you are good at studies!");
 }
+nice("Vaidehi");
+
 nice("Parthvi");
