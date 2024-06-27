86d36c959e61172dbda2965dba97e71d90ed0dc1
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-27 13:52:07 +0530

    v5

--- Video 5/index.js
+++ Video 5/index.js
@@ -1,9 +1,9 @@
 // Functions
 
 function nice(name) {
-    console.log("Hey " + name + "I am nice!");
-    console.log("Hey " + name + "I am fine!");
-    console.log("Hey " + name + "your tshirt is nice!");
-    console.log("Hey " + name + "you are good at studies!");
+    console.log("Hey " + name + " I am nice!");
+    console.log("Hey " + name + " I am fine!");
+    console.log("Hey " + name + " your tshirt is nice!");
+    console.log("Hey " + name + " you are good at studies!");
 }
-
+nice("Parthvi");
