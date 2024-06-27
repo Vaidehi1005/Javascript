2b573f971ba476d250042c9f982c1bf48c6f7df7
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-27 13:43:02 +0530

    v3

--- Video 5/index.js
+++ Video 5/index.js
@@ -1,1 +1,5 @@
 // Functions
+console.log("Hey Vaidehi I am nice!");
+console.log("Hey Vaidehi I am fine!");
+console.log("Hey Vaidehi your tshirt is nice!");
+console.log("Hey Vaidehi you are good at studies!");
