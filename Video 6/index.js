da7b00ff886e6f62271842e47e35d3c1c2f55884
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-27 16:38:56 +0530

    v6

--- Video 6/index.js
+++ Video 6/index.js
@@ -1,1 +1,10 @@
 console.log("This is string tutorial");
+let a = "Vaidehi";//this a var has string bcoz it have a double quotes and the collection of characters are called string and it is always written in " ".
+console.log(a);//it is printing the whole string.
+console.log(a[0]);//it access 1st char 
+console.log(a[1]);//it access 2st char 
+console.log(a[2]);//it access 3st char 
+console.log(a[3]);//it access 4st char 
+console.log(a[4]);//it access 5st char 
+console.log(a[5]);//it access 6st char 
+console.log(a[6]);//it access 7st char 
