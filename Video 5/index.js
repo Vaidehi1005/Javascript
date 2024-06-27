2f94daee9ac51c691099169bd51aaa6072f18ed6
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-27 14:05:32 +0530

    v5

--- Video 5/index.js
+++ Video 5/index.js
@@ -6,11 +6,16 @@ function nice(name) {
     console.log("Hey " + name + " your tshirt is nice!");
     console.log("Hey " + name + " you are good at studies!");
 }
-nice("Vaidehi");
+// nice("Vaidehi");
 
-nice("Parthvi");
+// nice("Parthvi");
 
-function sum(a,b) {
-    console.log(a+b);//this is the void fun which is not returning any value and just printing the value
+function sum(a, b) {
+    // console.log(a+b);//this is the void fun which is not returning any value and just printing the value
+    return a + b;//this is for returning the integer value when passing the int value and storing into result and then we will print the result
 }
-sum(111,222);
+// sum(11,222);
+
+result = sum(22, 10);
+
+console.log("The sum of the number is: ", result);
