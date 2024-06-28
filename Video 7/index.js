16ca20bad4de57339e4b710501179b03f127910e
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-28 13:30:48 +0530

    v7

--- Video 7/index.js
+++ Video 7/index.js
@@ -4,5 +4,12 @@ Arrays are variables which can hold more than one value
 array ek var j che and js nodejs thi use karvu hoi to ena thi k web browser thi use kari saku
 */
 let arr=[1,2,4,5,7];
+//index= 0,1,2,3,4
+console.log(arr);
+console.log(arr.length)
+//arrays are mutable so we can change the array
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[7]);
 
 
