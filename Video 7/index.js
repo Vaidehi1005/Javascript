a6ee9b6859782301c8181b8b8f77d11340b07314
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-28 13:04:45 +0530

    v7

--- Video 7/index.js
+++ Video 7/index.js
@@ -1,4 +1,8 @@
 /*Arrays are the collection of multiple data types like:
 Number,String,Objects,etc.
+Arrays are variables which can hold more than one value
+array ek var j che and js nodejs thi use karvu hoi to ena thi k web browser thi use kari saku
 */
+let arr=[1,2,4,5,7];
+
 
