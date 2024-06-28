fc1d7d308792737f1d2b73428747f32c056e010d
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-28 13:55:00 +0530

    v7

--- Video 7/index.js
+++ Video 7/index.js
@@ -3,14 +3,17 @@ Number,String,Objects,etc.
 Arrays are variables which can hold more than one value
 array ek var j che and js nodejs thi use karvu hoi to ena thi k web browser thi use kari saku
 */
-let arr=[1,2,4,5,7];
+let arr = [1, 2, 4, 5, 7];
 //index= 0,1,2,3,4
-arr[0]=5666;
-console.log(arr,typeof arr);
-console.log(arr.length)
-//arrays are mutable so we can change the array
-console.log(arr[0]);
-console.log(arr[2]);
-console.log(arr[4]);
+arr[0] = 5666;
+// console.log(arr,typeof arr);
+// console.log(arr.length)
+// //arrays are mutable so we can change the array
+// console.log(arr[0]);
+// console.log(arr[2]);
+// console.log(arr[4]);
 
+//array methods
+console.log(arr.toString());//always string aapane commas ma j male jm k A,P,P,L,E="APPLE"
+console.log(arr.join(" and "));//aa array ma je no che ene join karva che and thi to join ma and lakhvanu then e badha no ek bija sathe comma thi nai and thi join thai jase
 
