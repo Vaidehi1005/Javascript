8becdc4802eafcf1ace6c30dc6bcdedb798d238f
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-30 15:44:35 +0530

    v7

--- Video 7/mfr.js
+++ Video 7/mfr.js
@@ -7,7 +7,7 @@ let arr = [1, 13, 5, 7, 11];
 //it is the another way of doing the above is mapping
 
 let newArr = arr.map((e, index, array) => {
-    return e ** 2
+    return e ** 2//e is for the element/value is the var that acts as a loop fun for the all the elements of the array
 })
 console.log(newArr);
 
