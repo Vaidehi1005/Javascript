a543ae67c901449b0e49b4bda75a63c2e908e6f7
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-30 15:40:07 +0530

    v7

--- Video 7/mfr.js
+++ Video 7/mfr.js
@@ -4,6 +4,7 @@ let arr = [1, 13, 5, 7, 11];
 //     const element = arr[index];
 //     newArr.push(element ** 2)
 // }
+//it is the another way of doing the above is mapping
 
 let newArr = arr.map((e, index, array) => {
     return e ** 2
