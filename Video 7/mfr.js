23c2cb66a527c28c78a49eedbfe40d229ae9ada2
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-06-30 16:15:33 +0530

    v7

--- Video 7/mfr.js
+++ Video 7/mfr.js
@@ -1,4 +1,5 @@
 let arr = [1, 13, 5, 7, 11];
+//map loop
 // let newArr = []
 // for (let index = 0; index < arr.length; index++) {
 //     const element = arr[index];
@@ -6,9 +7,16 @@ let arr = [1, 13, 5, 7, 11];
 // }
 //it is the another way of doing the above is mapping
 
-let newArr = arr.map((e, index, array) => {
-    return e ** 2//e is for the element/value is the var that acts as a loop fun for the all the elements of the array
-})
-console.log(newArr);
+// let newArr = arr.map((e, index, array) => {
+//     return e ** 2//e is for the element/value is the var that acts as a loop fun for the all the elements of the array
+// })
+// console.log(newArr);
+//filter loop
 
-newArr.filter
+ const greaterThanSeven =(value)=>{
+    if(value>7){
+        return true
+    }
+    return false
+ }
+console.log(newArr.filter(greaterThanSeven))
