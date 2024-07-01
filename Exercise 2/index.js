6159230a3b8fb081d8ebe4b42b43bf479858816a
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-01 16:34:51 +0530

    e2

--- Exercise 2/index.js
+++ Exercise 2/index.js
@@ -12,3 +12,36 @@ Bros
 Limited
 Hub
 */
+let random = Math.random()
+// 0 0.33 0.66 1 (3 intervals ma todi didhu )
+let first,second,third;
+//Lets generate the first word
+if(random<0.33){
+ first="Crazy"
+}
+else if(random<0.66 && random>=0.33){
+    first="Amazing"
+}
+else{
+    first="Fire"
+}
+//Lets generate the second word
+if(random<0.33){
+    second="Engine"
+}
+else if(random<0.66 && random>=0.33){
+    second="Foods"
+}
+else{
+    second="Garments"
+}
+//Lets generate the third word
+if(random<0.33){
+    third="Bros"
+}
+else if(random<0.66 && random>=0.33){
+    third="Limited"
+}
+else{
+    third="Hub"
+}
