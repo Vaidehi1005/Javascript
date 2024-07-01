4c29af70410e8002ee568d6a95d3e7ae295f9b69
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-01 14:36:27 +0530

    e1

--- Exercise 1/index.js
+++ Exercise 1/index.js
@@ -9,4 +9,24 @@ This faulty does the following:
 / ---> **
 It performs the wrong operations 10% of the times
 */
-let random = Math.random()
+let random = Math.random()// ek random var banayo jema ek random no generate thase aa fun thi 0 to 1 na between
+let a = prompt("Enter first number")
+let b = prompt("Enter operation")
+let c = prompt("Enter second number")
+// aapde aek object banaisu je object ma store karie che  kone su operation karvanu che e store karie che key values pair ma
+let obj = {
+    "+":"-",
+    "-":"+",
+    "*":"/",
+    "/":"**",
+}
+if(random>0.1){
+    //Perform correct calculation
+    
+    
+}
+else{
+    //Perform wrong operations
+    b=obj[b]
+
+}
