30b6814e273ac67c111e630fb9586fd5deb0fb0f
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-01 10:51:16 +0530

    v7

--- Video 7/mfr.js
+++ Video 7/mfr.js
@@ -1,30 +1,32 @@
 let arr = [1, 13, 5, 7, 11];
 //map loop
-// let newArr = []
-// for (let index = 0; index < arr.length; index++) {
-//     const element = arr[index];
-//     newArr.push(element ** 2)
-// }
+let newArr = []
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    newArr.push(element ** 2)
+}
 //it is the another way of doing the above is mapping
 
-// let newArr = arr.map((e, index, array) => {
-//     return e ** 2//e is for the element/value is the var that acts as a loop fun for the all the elements of the array
-// })
-// console.log(newArr);
+let newArr1 = arr.map((e, index, array) => {
+    return e ** 2//e is for the element/value is the var that acts as a loop fun for the all the elements of the array
+})
+console.log(newArr1);
 //filter loop
 
-// const greaterThanSeven = (value) => {
-//     if (value > 7) {
-//         return true
-//     }
-//     return false
-// }
-// console.log(newArr.filter(greaterThanSeven))
+const greaterThanSeven = (value) => {
+    if (value > 7) {
+        return true
+    }
+    return false
+}
+console.log(newArr.filter(greaterThanSeven))
 
 //reduce method-reduces an array to single value
 let arr2 = [1, 2, 3, 4, 5]
 
 const red = (a, b) => {
-    return a * b
+    return a + b
 }
 console.log(arr2.reduce(red))
+
+Array.from("Parthvi")
