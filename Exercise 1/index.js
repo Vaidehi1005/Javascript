cb4c29b4158d6952b696ef30f3fe8459c4d7bd3a
Vaidehi1005 <vaidehi.nayakpura@gmail.com>
2024-07-01 13:44:33 +0530

    e1

---
+++ Exercise 1/index.js
@@ -0,0 +1,11 @@
+/* Create a faulty calculator using Javascript
+
+This faulty does the following:
+1.It takes two numbers  as input from the user
+2.It performs wrong operations as follows:
++ ---> -
+- ---> +
+* ---> /
+/ ---> **
+It performs the wrong operations 10% of the times
+*/
